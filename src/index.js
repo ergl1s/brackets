192d16fe6a745ea913777fb7db571b2164a58944
ergl1s <bibikov2000@mail.ru>
2020-03-09 22:57:59 +0300

    feat: final solution of the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let list = []; 
+  let flag = false; 
+  for(let i = 0; i < str.length; i++) {
+    flag = false; 
+    for(let j = 0; j < bracketsConfig.length; j++) {
+      if (str[i] == bracketsConfig[j][1]) {
+        if (list[list.length - 1] == bracketsConfig[j][0]) {
+          flag = true;
+          list.pop();   
+        }
+        break;
+      }
+    } 
+    if (!flag) {
+      for(let j = 0; j < bracketsConfig.length; j++) {
+        if (str[i] == bracketsConfig[j][0]) {
+          list.push(str[i]); 
+          flag = true; 
+          break; 
+        }
+      } 
+    }
+    if (!flag) return false; 
+  }
+  if (list.length == 0) return true;
+  else return false; 
 }
